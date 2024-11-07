65a0fd2486a385a0d661ff47eb9a7310245fef64
krish736 <112505529+krish736@users.noreply.github.com>
2024-11-07 15:53:57 +0530

    commit

--- client/src/pages/Rooms.jsx
+++ client/src/pages/Rooms.jsx
@@ -1,6 +1,37 @@
-import React from 'react'
+import React from "react";
+import Cards from "../components/Cards";
+import {rooms} from "../data/roomsData.js";
 
 export default function Rooms() {
-  return <div>Rooms</div>;
-}
+  return (
+    <div className="mb-10">
+      <h1 className="border-b-2 mx-10 mt-20 text-3xl">Location - Indore</h1>
+      <div className="grid lg:grid-cols-3 gap-5 mx-10 mt-10">
+        {rooms.map((room, index) => (
+          <Cards
+            key={index}
+            image={room.img}
+            rating={room.rating}
+            title={room.title}
+            description={room.description}
+            address={room.address}
+          />
+        ))}
+      </div>
 
+      <h1 className="border-b-2 mx-10 mt-20 text-3xl">Location - MHOW</h1>
+      <div className="grid lg:grid-cols-3 gap-5 mx-10 mt-10">
+        {rooms.map((room, index) => (
+          <Cards
+            key={index}
+            image={room.img}
+            rating={room.rating}
+            title={room.title}
+            description={room.description}
+            address={room.address}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
