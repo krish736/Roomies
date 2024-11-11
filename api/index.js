4066d357c9bebc18adc52a11b15d1826fb2b29fc
Juned Ahmed Choudhary <jjahmedchoudhary@gmail.com>
2024-11-11 20:14:11 +0530

    added the mouse follower and the room page logic with consoles

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors"
 import authRouter from "./routes/auth.route.js";
 import userRouter from "./routes/user.route.js";
+import  locationRoutes from "./routes/location.route.js"
 import { errorHandler } from "./middleware/error.middleware.js";
 
 const app = express();
@@ -23,6 +25,8 @@ app.listen(port, () => {
   console.log("Server is Listening!");
 });
 
+app.use(cors());
+
 app.use(express.json());
 
 app.get("/test", (req, res) => {
@@ -31,4 +35,13 @@ app.get("/test", (req, res) => {
 
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
+app.use("/api", locationRoutes)
+
+app.use(cors({
+  origin: '*', // Replace with your frontend URL
+  methods: ['GET', 'POST'], // Allow specific HTTP methods
+  credentials: true // Allow credentials like cookies if needed
+}));
+
+
 app.use(errorHandler);
